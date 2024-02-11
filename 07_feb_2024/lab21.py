3cac6a5cb730f2d728b501623a9386e637ae5e6e
pranav <guptapranav2504@gmail.com>
2024-02-11 15:22:48 +0530

    07 feb

---
+++ 07_feb_2024/lab21.py
@@ -0,0 +1,11 @@
+isPranavMarried = True
+isPranavMarried = False
+
+is_Pranav_Married = False
+
+print(list)
+print(type(list))
+
+set={1,2,3,3}
+print(set)
+print(type(set))
---
+++ 07_feb_2024/lab23.py
@@ -0,0 +1,9 @@
+#literal
+
+#var_name = var_value
+a=10
+
+
+name="pranav"
+v='C'
+print(type(v))
